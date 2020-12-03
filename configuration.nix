1301240b5a3abcaa074411a5fc7568022a1cab5a
Jon Roberts <jon@emptyflask.net>
2020-12-03 13:11:15 -0600

    keep outputs for projects (direnv 'use nix')

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,11 @@
     ubuntu_font_family
   ];
 
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
