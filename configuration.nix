8cd90e3a52c13d787d4caf8d360c8173f133670c
Jon Roberts <jon@emptyflask.net>
2020-12-03 13:12:10 -0600

    auto upgrade

--- configuration.nix
+++ configuration.nix
@@ -189,4 +189,5 @@
   # servers. You should change this only after NixOS release notes say you
   # should.
   system.stateVersion = "18.09"; # Did you read the comment?
+  system.autoUpgrade.enable = true;
 }
