07bf21883fb7fe49806ac3dd61a4f3f968caefb1
Jon Roberts <jon@emptyflask.net>
2022-05-07 21:24:05 -0500

    add reflex-frp cache

--- configuration.nix
+++ configuration.nix
@@ -80,8 +80,18 @@ in
   };
 
   nix = {
-    binaryCaches          = [ "https://hydra.iohk.io" "https://iohk.cachix.org" ];
-    binaryCachePublicKeys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo=" ];
+    binaryCaches          = [
+      "https://cache.nixos.org/"
+      "https://hydra.iohk.io"
+      # "https://iohk.cachix.org"
+      "https://nixcache.reflex-frp.org"
+
+    ];
+    binaryCachePublicKeys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+      # "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+      "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI="
+    ];
   };
 
   console = {
